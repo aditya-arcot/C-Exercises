2107ee00f8bf2a52f84ce9afd0eb40b3736ff261
aditya-arcot <arcot.aditya@utexas.edu>
2024-12-22 13:41:31 -0600

    remove comments program

---
+++ 01_intro/21_remove_comments.c
@@ -0,0 +1,116 @@
+// remove all comments from C program input
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int prev_ch;
+int ch = -1;
+
+int in_string = FALSE;
+int in_char = FALSE;
+int in_single_comment = FALSE;
+int in_multi_comment = FALSE;
+int escaped = FALSE;
+
+int handle_char();
+
+int main()
+{
+    while (TRUE)
+    {
+        prev_ch = ch;
+        ch = getchar();
+        if (ch == EOF) break;
+        if (handle_char()) break;
+    }
+}
+
+int handle_char()
+{
+    if (in_single_comment)
+    {
+        if (ch == '\n')
+        {
+            putchar(ch);
+            in_single_comment = FALSE;
+        }
+        return 0;
+    }
+
+    if (in_multi_comment)
+    {
+        if (ch == '*') {
+            prev_ch = ch;
+            ch = getchar();
+            if (ch == EOF) return 1;
+
+            if (ch == '/')
+            {
+                in_multi_comment = FALSE;
+                return 0;
+            }
+        }
+        return 0;
+    }
+
+    if (escaped) {
+        escaped = FALSE;
+        putchar(ch);
+        return 0;
+    }
+
+    if (ch == '\\') {
+        escaped = TRUE;
+        putchar(ch);
+        return 0;
+    }
+
+    if (ch == '"' && !escaped) {
+        in_string = !in_string;
+        putchar(ch);
+        return 0;
+    }
+    
+    if (in_string) {
+        putchar(ch);
+        return 0;
+    }
+
+    if (ch == '\'' && !escaped) {
+        in_char = !in_char;
+        putchar(ch);
+        return 0;
+    }
+
+    if (in_char) {
+        putchar(ch);
+        return 0;
+    }
+
+    if (ch == '/') {
+        prev_ch = ch;
+        ch = getchar();
+        if (ch == EOF) return 1;
+
+        if (ch == '/')
+        {
+            in_single_comment = TRUE;
+            return 0;
+        }
+
+        if (ch == '*')
+        {
+            in_multi_comment = TRUE;
+            return 0;
+        }
+
+        putchar(prev_ch);
+        putchar(ch);
+        return 0;
+    }
+
+    putchar(ch);
+    return 0;
+}
