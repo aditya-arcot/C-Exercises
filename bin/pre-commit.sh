4e3d8608839984d5987803436b1954db6be2c546
aditya-arcot <dev@aarcot.com>
2025-08-21 15:04:14 -0500

    use separate hook file with sh extension for recognition by formatter

---
+++ bin/pre-commit.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -euo pipefail
+
+STASHED=false
+
+cleanup() {
+    if $STASHED; then
+        echo "Restoring stashed changes"
+        git stash pop -q
+    fi
+}
+trap cleanup EXIT
+
+echo "Checking for unstaged changes"
+if ! git diff --quiet; then
+    echo "Stashing unstaged changes"
+    git stash -k
+    STASHED=true
+fi
+
+STAGED_BASH_FILES=$(git diff --cached --name-only --diff-filter=AM | grep -E '\.sh$' || true)
+if [ -n "$STAGED_BASH_FILES" ]; then
+    echo "Formatting Bash files"
+    echo "$STAGED_BASH_FILES" | xargs format_bash.sh
+    echo "$STAGED_BASH_FILES" | xargs git add
+    echo
+
+    echo "Linting Bash files"
+    echo "$STAGED_BASH_FILES" | xargs lint_bash.sh
+    echo
+fi
+
+STAGED_C_FILES=$(git diff --cached --name-only --diff-filter=AM | grep -E '\.(c|h)$' || true)
+if [ -n "$STAGED_C_FILES" ]; then
+    echo "Formatting C files"
+    echo "$STAGED_C_FILES" | xargs format_c.sh
+    echo "$STAGED_C_FILES" | xargs git add
+    echo
+fi
