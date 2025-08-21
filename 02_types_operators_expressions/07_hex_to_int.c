932c15c0ab52886dc908e9e6e48fc36ba76d718c
aditya-arcot <dev@aarcot.com>
2025-08-21 15:34:52 -0500

    simplify htoi code

--- 02_types_operators_expressions/07_hex_to_int.c
+++ 02_types_operators_expressions/07_hex_to_int.c
@@ -12,7 +12,6 @@
 
 int htoi(char s[]);
 int is_whitespace(int ch);
-int recalculate_int(int curr, int xdigit);
 
 int main(void) {
     char s[] = "0x1f";
@@ -20,55 +19,37 @@ int main(void) {
 
     char s2[] = " FF";
     printf("%s - %d\n", s2, htoi(s2));
+
+    char s3[] = "052";
+    printf("%s - %d\n", s3, htoi(s3));
 }
 
 int htoi(char s[]) {
-    bool start = true;
+    int i = 0, n = 0;
 
-    int n = 0;
-    bool prefix = true;
-    for (int i = 0; s[i] != EOF; i++) {
-        if (is_whitespace(s[i])) {
-            if (start)
-                continue;
-            else
-                return n;
-        }
-        start = false;
+    // strip leading whitespace
+    while (is_whitespace(s[i]))
+        i++;
 
-        if (prefix) {
-            prefix = false;
-            if (s[i] == '0') {
-                ++i;
-                if (s[i] == 'x' || s[i] == 'X')
-                    continue;
-                if (!isxdigit(s[i]))
-                    return n;
-                n = recalculate_int(n, s[i]);
-                continue;
-            }
-            if (!isxdigit(s[i]))
-                return n;
-            n = recalculate_int(n, s[i]);
-            continue;
-        }
-        if (!isxdigit(s[i]))
-            return n;
-        n = recalculate_int(n, s[i]);
+    // strip hex prefix
+    if (s[i] == '0' && (s[i + 1] == 'x' || s[i + 1] == 'X')) {
+        i += 2;
+    }
+
+    // parse hex digits
+    for (int ch; (ch = s[i++]) != '\0';) {
+        if (!isxdigit(ch))
+            break;
+        if (ch >= '0' && ch <= '9')
+            n = n * 16 + (ch - '0');
+        else if (ch >= 'a' && ch <= 'f')
+            n = n * 16 + (ch - 'a' + 10);
+        else
+            n = n * 16 + (ch - 'A' + 10);
     }
     return n;
 }
 
 int is_whitespace(int ch) {
     return ch == ' ' || ch == '\t' || ch == '\n';
-}
-
-int recalculate_int(int curr, int xdigit) {
-    if (!isxdigit(xdigit))
-        return curr;
-    if (xdigit >= '0' && xdigit <= '9')
-        return curr * 16 + (xdigit - '0');
-    if (xdigit >= 'a' && xdigit <= 'f')
-        return curr * 16 + (xdigit - 'a' + 10);
-    return curr * 16 + (xdigit - 'A' + 10);
 }
