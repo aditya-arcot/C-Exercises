77e42d213e17d6fb14176836b2181ecb87d4f9f0
aditya-arcot <dev@aarcot.com>
2025-09-15 11:05:40 -0500

    fix expand program

--- 03_control_flow/06_expand.c
+++ 03_control_flow/06_expand.c
@@ -10,7 +10,7 @@
 void expand(char s1[], char s2[]);
 
 int main(void) {
-    char s1[] = "a-f f-a 0-5 5-0 a-b-c-e-z --0";
+    char s1[] = "a-f f-a 0-5 5-0 a-b-c-e-z *-0 a-";
     char s2[128];
     expand(s1, s2);
     printf("%s\n", s2);
@@ -19,7 +19,11 @@ int main(void) {
 void expand(char s1[], char s2[]) {
     int i = 0, j = 0;
     for (int ch; (ch = s1[i++]) != NULL_CHAR;) {
-        if (s1[i] == '-' && s1[i + 1] >= ch) {
+        if (ch == ' ') {
+            s2[j++] = ch;
+            continue;
+        }
+        if (s1[i] == '-' && s1[i + 1] != NULL_CHAR && s1[i + 1] >= ch) {
             while (ch <= s1[i + 1])
                 s2[j++] = ch++;
             i += 2;
